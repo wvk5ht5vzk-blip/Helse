5bb22451b154af16310c0b2eeed3bb6fa88dd191
wvk5ht5vzk-blip <anders_sand93@hotmail.com>
2026-06-23 11:37:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const sliders = [
+    "sleep",
+    "mood",
+    "energy",
+    "stress",
+    "activity"
+];
 
+sliders.forEach(id => {
+
+    const slider = document.getElementById(id);
+    const value = document.getElementById(id + "Value");
+
+    slider.addEventListener("input", () => {
+        value.textContent = slider.value;
+    });
+
+});
